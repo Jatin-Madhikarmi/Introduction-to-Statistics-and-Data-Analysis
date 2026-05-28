1f571e21d41a15d40c5637169525bfe38c869a61
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-05-28 12:20:22 +0545

    Added some comments for better readibility and changed the file's name

--- MeanStds.py
+++ MeanStds.py
@@ -1,7 +1,13 @@
 import numpy as np
 
+# Here we specify the value of the seed number becuase we want to reproduce the same result again and again if we omit this line then
+# everytime we get a new values for our poulation_data and hence all the other values like mean and standard deviation of populatin and
+# sample would differ
 np.random.seed(42)
 
+
+# In a Poisson Distribution which is used to determine how many events occurs in a given amount of time or space, the parameter lamda 
+# represents both the expected average and variance of the distribution  
 lamda_val=10
 population_size=10000
 
